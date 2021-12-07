af92fcd38a0db767b76d994f31767aafa6a85f4f
dennyabrain <denny.george90@gmail.com>
2021-12-07 09:35:17 +0530

    feat : add seeder for internal beta.
    note : no data file is present so this will most likely not work as is.

--- annotators/backend/sequelize/seeders/20211130053510-beta-3.js
+++ annotators/backend/sequelize/seeders/20211130053510-beta-3.js
@@ -1,6 +1,5 @@
 "use strict";
 const { v4: uuidv4 } = require("uuid");
-const { Op } = require("Sequelize");
 const generatePassword = require("password-generator");
 const data = require("./posts_beta_3_en_tamil_130.json");
 
