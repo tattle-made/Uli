e19ce640609e0d2668d7cb7793d198e9cfb86bbb
Denny George <denny.george90@gmail.com>
2022-01-11 16:07:07 +0530

    feat : changes for beta 5
    * feat: add script to add tamil users
    * feat: add beta 5 dataset. 
    * fix pending count issue

--- annotators/backend/sequelize/seeders/20211207040127-beta-4_50_en.js
+++ annotators/backend/sequelize/seeders/20211207040127-beta-4_50_en.js
@@ -1,8 +1,7 @@
 "use strict";
-
 const { v4: uuidv4 } = require("uuid");
 const generatePassword = require("password-generator");
-const data = require("./posts_beta_3_en_tamil_130.json");
+const data = require("./posts_beta_4_en_tam_50_30.json");
 
 /**
  * This seeder was used in the internal beta. it inserts 50 english posts in the db
@@ -17,12 +16,149 @@ console.log(time_of_run);
 
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		// GET ENGLISH USERS
+		// INSERT ENGLISH ANNOTATORS
+		const users = await queryInterface.bulkInsert("Users", [
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "tarunima",
+				password: generatePassword(),
+				lang: "en",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "cheshta",
+				password: generatePassword(),
+				lang: "en",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "ambika",
+				password: generatePassword(),
+				lang: "en",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "brindaa",
+				password: generatePassword(),
+				lang: "en",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "arnav",
+				password: generatePassword(),
+				lang: "en",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "denny",
+				password: generatePassword(),
+				lang: "en",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+		]);
 		const doubleUserIds = await queryInterface.sequelize.query(
 			`SELECT id from Users where lang='en';`
 		);
 		const englishUserIDs = doubleUserIds[0];
 		console.log(englishUserIDs);
+
+		// INSERT TAMIL ANNOTATORS
+		await queryInterface.bulkInsert("Users", [
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "brindaa_ta",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "denny_ta",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "vvveerasekar",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "sonia.arunkumar",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "gracebanug",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "Bharathy10",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "dharinirj24",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+			{
+				id: uuidv4(),
+				role: "annotator",
+				username: "atimsrainnav",
+				password: generatePassword(),
+				lang: "ta",
+				createdAt: time_of_run,
+				updatedAt: time_of_run,
+			},
+		]);
+		const doubleTamilUserIds = await queryInterface.sequelize.query(
+			`SELECT id from Users where lang='ta';`
+		);
+		const tamilUserIDs = doubleTamilUserIds[0];
+		console.log(tamilUserIDs);
 
 		// INSERT POSTS
 		for (var i = 0; i < data.posts.length; i++) {
@@ -63,7 +199,35 @@ module.exports = {
 				]);
 			}
 		}
+
+		// ALLOCATE POSTS TO TAMIL ANNOTATORS
+		const doubleTamilPostIds = await queryInterface.sequelize.query(
+			`SELECT id from Posts WHERE lang="ta";`
+		);
+		const tamilPostIDs = doubleTamilPostIds[0];
+		console.log(`New tamil posts count : ${tamilPostIDs.length}`);
+		console.log(`inserting for TA users`);
+		for (var j = 0; j < tamilPostIDs.length; j++) {
+			for (var i = 0; i < tamilUserIDs.length; i++) {
+				await queryInterface.bulkInsert("UserPostAllocations", [
+					{
+						id: uuidv4(),
+						userId: tamilUserIDs[i].id,
+						postId: tamilPostIDs[j].id,
+						status: "pending",
+						createdAt: time_of_run,
+						updatedAt: time_of_run,
+					},
+				]);
+			}
+		}
 	},
 
-	down: async (queryInterface, Sequelize) => {},
+	down: async (queryInterface, Sequelize) => {
+		return Promise.all([
+			queryInterface.bulkDelete("Users", null, {}),
+			await queryInterface.bulkDelete("Posts", null, {}),
+			await queryInterface.bulkDelete("UserPostAllocations", null, {}),
+		]);
+	},
 };
