22668047bdc550b898214a796ed0430f3acecc1e
Denny George <denny.george90@gmail.com>
2022-01-14 16:25:39 +0530

    Revert "feat : Add Documentation for Backend & DB Setup"
    This reverts commit 959b6098fa3d8cbcc8cceaaf0b45bc9ee2f30529.

--- annotators/backend/sequelize/config/config.js
+++ annotators/backend/sequelize/config/config.js
@@ -4,7 +4,6 @@ module.exports = {
 		password: "tattle_pw",
 		database: "ogbv_annotator_local",
 		host: "127.0.0.1",
-		port: 3306, /* default port for mysql is 3306, update it if you are using another port */
 		dialect: "mysql",
 	},
 	test: {
