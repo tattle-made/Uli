e2debeb2909d4f81838b9272f777592e127d8184
Denny <denny.george90@gmail.com>
2022-02-08 07:10:29 +0530

    chore : add user and allocation data for external beta testers

--- annotators/backend/temp/script-post-beta-csv-to-json.js
+++ annotators/backend/temp/script-post-beta-csv-to-json.js
@@ -6,21 +6,13 @@ const results = [];
 
 // console.log(data.posts[0]);
 (async function process() {
-<<<<<<< HEAD
-	fs.createReadStream("posts_beta_7.csv")
-=======
-	fs.createReadStream("posts_beta_5_en.csv")
->>>>>>> 959b6098fa3d8cbcc8cceaaf0b45bc9ee2f30529
-		.pipe(csv())
-		.on("data", (data) => results.push(data))
-		.on("end", async () => {
-			await writeFile(
-<<<<<<< HEAD
-				"posts_beta_7.json",
-=======
-				"posts_beta_5_en.json",
->>>>>>> 959b6098fa3d8cbcc8cceaaf0b45bc9ee2f30529
-				JSON.stringify({ posts: results })
-			);
-		});
+  fs.createReadStream("alllang_tweets.csv")
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", async () => {
+      await writeFile(
+        "alllang_tweets.json",
+        JSON.stringify({ posts: results })
+      );
+    });
 })();
