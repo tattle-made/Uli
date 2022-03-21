99de0b5082db9f1c672003aaeda6435fa993e742
Denny <denny.george90@gmail.com>
2022-03-21 15:23:00 +0530

    feat : init

--- browser extension/plugin/react/content-script.js
+++ browser extension/plugin/react/content-script.js
@@ -1,10 +1,17 @@
 import ReactDOM from "react-dom";
+import { Box, Text } from "grommet";
 import { InlineButtons } from "./InlineButtons";
 import { replaceSlur, updateSlurList } from "./slur-replace";
 let currentTweetCount = 0;
 import repository from "./repository";
 const { getPreferenceData } = repository;
 import chrome from "./chrome";
+
+const TestComponent = () => (
+  <Box round border>
+    <Text>Hello</Text>
+  </Box>
+);
 
 // console.log("Content Script Loaded Again");
 // if (document.readyState === "loading") {
@@ -23,22 +30,47 @@ setTimeout(async () => {
   await initialize();
 }, 3000);
 
-const processTweets = async function () {
-  const userData = await repository.getUserData();
-  const preferenceData = await repository.getPreferenceData();
-  // console.log({ userData, preferenceData });
-  spans = document.getElementsByClassName(
-    "css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"
-  );
-  spans = Array.from(spans);
-  if (spans.length != currentTweetCount) {
-    console.log("replacing slur");
-    currentTweetCount = spans.length;
-    spans.map((span) => {
-      text = span.innerText;
-      span.innerText = replaceSlur(text);
-    });
+const processTweets = async function (mutationsList, observer) {
+  console.log("process tweets");
+  // console.log(mutationsList);
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      console.log("A child node has been added or removed.");
+      console.log(mutation);
+      // console.log(typeof mutation);
+      const nodes = Array.from(mutation.addedNodes);
+      nodes.map((node) => {
+        const id = `ogbv_tweet_${Math.floor(Math.random() * 999999)}`;
+        console.log(id, node.innerText);
+        node.setAttribute("id", id);
+        // ReactDOM.render(<TestComponent />, node);
+      });
+    } else if (mutation.type === "attributes") {
+      // console.log("The " + mutation.attributeName + " attribute was modified.");
+    }
   }
+  // const tweets = document.getElementsByTagName("article");
+
+  // console.log("-----------------");
+  // console.log(`found ${tweets.length} tweets`);
+  // // console.log(spans);
+  // console.log("-----------------");
+
+  // const userData = await repository.getUserData();
+  // const preferenceData = await repository.getPreferenceData();
+  // // console.log({ userData, preferenceData });
+  // spans = document.getElementsByClassName(
+  //   "css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"
+  // );
+  // spans = Array.from(spans);
+  // if (spans.length != currentTweetCount) {
+  //   console.log("replacing slur");
+  //   currentTweetCount = spans.length;
+  //   spans.map((span) => {
+  //     text = span.innerText;
+  //     span.innerText = replaceSlur(text);
+  //   });
+  // }
 };
 
 async function initialize() {
@@ -59,9 +91,11 @@ async function initialize() {
   }
 
   const observer = new MutationObserver(processTweets);
-  const bodyNode = document.getElementsByTagName("body")[0];
-  const config = { attributes: true, childList: true, subtree: true };
-  observer.observe(bodyNode, config);
+  const timeline = document.querySelector(
+    '[aria-label="Timeline: Conversation"]'
+  ).firstChild;
+  const config = { attributes: true, childList: true, subtree: false };
+  observer.observe(timeline, config);
 }
 
 chrome.addListener(
