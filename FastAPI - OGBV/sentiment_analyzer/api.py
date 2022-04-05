26982f8fe97522f7d0f93293eb81e448759bfaa1
Denny George <denny.george90@gmail.com>
2022-04-05 12:18:06 +0530

    feat : add json payload support for rest server (#80)

--- FastAPI - OGBV/sentiment_analyzer/api.py
+++ FastAPI - OGBV/sentiment_analyzer/api.py
@@ -2,6 +2,8 @@ from typing import Dict
 
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+
 
 from .classifier.model import Model, get_model
 
@@ -24,9 +26,10 @@ class OGBVResponse(BaseModel):
 
 @app.post("/predict", response_model=OGBVResponse)
 def predict(request: OGBVRequest,model: Model = Depends(get_model)):
-    #sentiment, confidence, probabilities = model.predict(request.text)
-    sentiment,confidence = model.predict(request.text)
+    payload_dict = jsonable_encoder(request)
+    print(payload_dict)
+    print(type(payload_dict))
+    sentiment,confidence = model.predict(payload_dict["text"])
     return OGBVResponse(
-    #   sentiment="neutral", confidence=0.99, probabilities=dict(negative=0.005,neutral=0.99,positive=0.005),
         sentiment = sentiment,confidence = confidence
     )
