2d5721fa98dbc002ec7de8f2604fca37f69d4299
Denny <denny.george90@gmail.com>
2022-05-03 10:24:17 +0530

    fix : change api endpoint

--- annotators/annotation-ui/src/components/config.js
+++ annotators/annotation-ui/src/components/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  api_endpoint: "https://ogbv-annotator.tattle.co.in/api", //"https://ogbv-annotator.tattle.co.in/api" or "https://ogbv-annotator-staging.tattle.co.in/api" or "http://localhost:3000/api"
+  api_endpoint: "https://ogbv-annotator.tattle-staging.co.in/api", //"https://ogbv-annotator.tattle.co.in/api" or "https://ogbv-annotator-staging.tattle.co.in/api" or "http://localhost:3000/api"
   annotator_url: "/app/post-annotator",
 };
