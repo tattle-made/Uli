2c1c5861cb86b487c2c058c06ecb6bc4c7a6cb92
Denny <denny.george90@gmail.com>
2022-05-17 23:09:03 +0530

    feat : add landing section and misc styling things

--- uli-website/src/pages/index.js
+++ uli-website/src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { useContext } from "react";
 import { Helmet } from "react-helmet";
-import { Grommet, Box } from "grommet";
+import { Grommet, Box, ResponsiveContext } from "grommet";
 import styled from "styled-components";
 
 // one image in each corner
@@ -90,7 +91,44 @@ const FeatureFollowUp = styled.p`
   margin-top: 0px;
 `;
 
+const LandingSectionHeading = styled.h1`
+  font-family: millisime, sans-serif;
+  font-size: 2em;
+  line-height: 1em;
+  font-style: normal;
+  font-weight: 200;
+`;
+
+const CTALink = styled.a`
+  background-color: red;
+  padding: 1em 3em 1em 3em;
+  width: 100px;
+  &:before {
+    content: "";
+    width: 100px;
+    height: 0;
+    position: relative;
+    top: 0;
+    left: 0;
+    border-bottom: 29px solid green;
+    border-left: 29px solid aqua;
+    border-right: 29px solid aqua;
+  }
+  &:after {
+    content: "";
+    width: 100px;
+    height: 0;
+    position: relative;
+    bottom: 0;
+    left: 0;
+    border-top: 29px solid red;
+    border-left: 29px solid #eee;
+    border-right: 29px solid #eee;
+  }
+`;
+
 const IndexPage = () => {
+  const size = useContext(ResponsiveContext);
   return (
     <Grommet theme={THEME}>
       <main>
@@ -102,6 +140,96 @@ const IndexPage = () => {
         </Helmet>
 
         <Box align="center">
+          <Box width={"large"} direction={"row-responsive"} align={"center"}>
+            <Box width={"4em"}>
+              <img src={"/Uli_Logo.png"} alt={"Uli Logo"} />
+            </Box>
+            <Box flex={"grow"} />
+            <Box direction="row" gap={"medium"}>
+              <p>About</p>
+              <p>User Guide</p>
+              <p>Team</p>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box align="center" margin={"large"}>
+          <Box width={"fit-content"} direction={"row-responsive"}>
+            <Box width={"18em"} alignSelf={"center"}>
+              <LandingSectionHeading>
+                Moderate Your Twitter Feed
+              </LandingSectionHeading>
+              <p>
+                Uli lets you take control over your Twitter timeline by
+                redacting slurs, allowing you to archive problematic tweets and
+                coordinating actions with your friends.
+              </p>
+              <a href={"/"}>Install Now</a>
+            </Box>
+            <Box width={"26em"}>
+              <img src={"/HeroIllustration.gif"}></img>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box align="center">
+          <h1>Features</h1>
+        </Box>
+
+        <Box align="center" margin={"large"}>
+          <Box
+            width={"fit-content"}
+            direction={"row-responsive"}
+            gap={"xlarge"}
+          >
+            <Box width={"small"} gap={"xsmall"} alignSelf={"center"}>
+              <FeatureLabel>ARCHIVE TWEETS</FeatureLabel>
+              <FeatureHeadline>
+                Archive tweets as evidence, to build a discourse or mobilise.
+              </FeatureHeadline>
+              <FeatureDescription>
+                Uli provides an easy mechanism to take screenshots of offending
+                tweets. These tweets can be stored locally or sent as an email
+                to yourself.
+              </FeatureDescription>
+              <FeatureFollowUp>
+                This crowdsourced list of offensive phrases is dynamic and each
+                user can gather around with Uli and help build a longer list.
+              </FeatureFollowUp>
+            </Box>
+            <Box width={"28em"}>
+              <img src={"/ArchiveTweet.gif"}></img>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box align="center" margin={"large"}>
+          <Box
+            width={"fit-content"}
+            direction={size === "small" ? "row-reverse" : "row-responsive"}
+            gap={"xlarge"}
+          >
+            <Box width={"28em"}>
+              <img src={"/SlurReplacement-HideAndSeekTweet.gif"}></img>
+            </Box>
+            <Box width={"small"} gap={"xsmall"} alignSelf={"center"}>
+              <FeatureLabel>{"SLUR REPLACEMENT" + size}</FeatureLabel>
+              <FeatureHeadline>
+                Automatic blurring of slurs in Hindi, English and Tamil
+              </FeatureHeadline>
+              <FeatureDescription>
+                Uli uses a crowdsourced list of slurs in Indian languages and
+                detects them in your tweet and hides them in real-time.
+              </FeatureDescription>
+              <FeatureFollowUp>
+                This crowdsourced list of offensive phrases is dynamic and each
+                user can gather around with Uli and help build a longer list.
+              </FeatureFollowUp>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box align="center" margin={"large"}>
           <Box
             width={"fit-content"}
             direction={"row-responsive"}
@@ -109,23 +237,20 @@ const IndexPage = () => {
           >
             <Box width={"small"} gap={"xsmall"} alignSelf={"center"}>
               <FeatureLabel>INVOKE NETWORK</FeatureLabel>
-              <FeatureHeadline>
-                Archive Tweets as evidence, to build a discource or mobilise.
-              </FeatureHeadline>
+              <FeatureHeadline>You are not alone in this.</FeatureHeadline>
               <FeatureDescription>
-                Uli provides an easy mechanism to take screenshot of offending
-                tweets. These tweets can be stored locally or sent as email to
-                you.
+                Involve your friends and community to act on problematic tweets
+                and combat online hate speech.
               </FeatureDescription>
               <FeatureFollowUp>
-                In future, we hope to build an anonymised public repository of
-                tweets archived by users. This archive will be our digital
-                courtyard where we cllect and narrate our experience of being on
-                social media.
+                This feature will invite people to support each other, share
+                stories, initiate conversations around intermediary
+                responsibility and interpersonal relationships and what it means
+                to be online.
               </FeatureFollowUp>
             </Box>
-            <Box width={"medium"}>
-              <img src={"/ArchiveTweet.gif"}></img>
+            <Box width={"28em"}>
+              <img src={"/InvokeNetworks.gif"}></img>
             </Box>
           </Box>
         </Box>
