a0302c903b7e72a263e1f9cc939ba146df9eab61
Denny <denny.george90@gmail.com>
2022-05-23 16:06:57 +0530

    feat : add plausible analytics and footer

--- uli-website/src/components/molecules/AppShell.jsx
+++ uli-website/src/components/molecules/AppShell.jsx
@@ -1,7 +1,7 @@
 import { Grommet, Box } from "grommet";
 import * as React from "react";
 import { Helmet } from "react-helmet";
-import { Theme } from "../atoms/UliCore";
+import { Colors, NavLink, Theme } from "../atoms/UliCore";
 import NavBar from "./NavBar";
 
 export default function AppShell({ children }) {
@@ -13,10 +13,22 @@ export default function AppShell({ children }) {
           <title>Uli</title>
           <link rel="stylesheet" href="layout.css" />
           <link rel="stylesheet" href="https://use.typekit.net/twt1ywc.css" />
+          <script
+            defer
+            data-domain="uli.tattle.co.in"
+            src="https://plausible.io/js/plausible.js"
+          ></script>
         </Helmet>
 
         <NavBar />
         {children}
+        <Box align="center" pad={"medium"}>
+          <Box direction="row" gap={"medium"}>
+            <NavLink to={"/privacy-policy"}>Privacy Policy</NavLink>
+            <NavLink to={"/blog"}>Blog</NavLink>
+            <NavLink to={"/about"}>About</NavLink>
+          </Box>
+        </Box>
       </main>
     </Grommet>
   );
