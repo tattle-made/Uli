3e8639b5313d6df395f0381a2dba23c5cec134df
Denny <denny.george90@gmail.com>
2022-05-29 17:26:32 +0530

    feat(browser extension) : add new style

--- browser extension/plugin/src/ui-components/atoms/Theme.jsx
+++ browser extension/plugin/src/ui-components/atoms/Theme.jsx
@@ -1,39 +1,48 @@
 const Theme = {
-  global: {
-    font: {
-      family: "Raleway",
+    global: {
+        font: {
+            family: 'millisime'
+        },
+        button: {
+            default: {
+                background: {
+                    color: '212121'
+                }
+            },
+            color: '#fdf6ed',
+            primary: {
+                background: {
+                    color: '#212121'
+                }
+            },
+            border: {
+                color: '#212121',
+                radius: '4em'
+            }
+        },
+        colors: {
+            background: '#fdf6ed',
+            brand: '#de8821',
+            'accent-1': '#212121',
+            text: {
+                dark: '#fdfffc',
+                light: '#212121'
+            }
+        },
+        focus: {
+            border: {
+                color: 'accent-1'
+            }
+        },
+        hover: {
+            background: 'accent-1'
+        }
     },
-    colors: {
-      background: "#ffffff",
-      brand: "#ff006e",
-      "accent-1": "#8338ec",
-      "accent-2": "#8338ec",
-      "accent-3": "#8338ec",
-      "neutral-1": "#ffbe0b",
-      "neutral-2": "#ffbe0b",
-      "neutral-3": "#ffbe0b",
-      "neutral-4": "#70234B",
-      "text-dark": "#edc9c4",
-      "text-light": "#fcbfa4",
-      text: {
-        dark: "#fdfffc",
-        light: "#4d194d",
-      },
-    },
-    focus: {
-      border: {
-        color: "accent-1",
-      },
-    },
-    hover: {
-      background: "accent-1",
-    },
-  },
-  button: {
-    border: {
-      radius: "0.2em",
-    },
-  },
+    button: {
+        border: {
+            radius: '0.2em'
+        }
+    }
 };
 
 export default Theme;
