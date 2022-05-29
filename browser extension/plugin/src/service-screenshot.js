84c116dd25c0a4985ee6d27396e72d4c99f5a5aa
Denny <denny.george90@gmail.com>
2022-05-29 17:27:15 +0530

    chore(browser extension): centralize screenshot feature

--- browser extension/plugin/src/service-screenshot.js
+++ browser extension/plugin/src/service-screenshot.js
@@ -1,18 +1,47 @@
-import domtoimage from "dom-to-image";
-import { saveAs } from "file-saver";
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
+import Api from './ui-components/pages/Api';
+const { uploadArchivedMedia } = Api;
 
-export function saveScreenshot(element) {
-  try {
-    return domtoimage
-      .toBlob(element.lastChild, { bgcolor: "white" })
-      .then(async function (blob) {
-        let fileName;
+export function saveScreenshot(element, storeLocally, accessToken) {
+    try {
+        return domtoimage
+            .toBlob(element.lastChild, { bgcolor: 'white' })
+            .then(async function (blob) {
+                let fileName;
+                const url = location.href;
 
-        fileName = new Date().toTimeString().split(" ").join("_");
+                try {
+                    console.log('----- 1');
+                    if (
+                        url.startsWith('https://twitter.com') &&
+                        url.search('/status/') != -1
+                    ) {
+                        console.log('----- 2');
+                        fileName = url.slice(url.search('/status/') + 8);
+                    } else {
+                        console.log('----- 3');
+                        fileName = new Date()
+                            .toTimeString()
+                            .split(' ')
+                            .join('_');
+                    }
+                } catch (err) {
+                    console.log('----- 4');
+                    console.log(err);
+                    fileName = new Date().toTimeString().split(' ').join('_');
+                }
+                if (storeLocally) {
+                    saveAs(blob, `ogbv_plugin_tweet_${fileName}.png`);
+                }
 
-        saveAs(blob, `ogbv_plugin_tweet_${fileName}.png`);
-      });
-  } catch (err) {
-    console.log("Could not save Image", err);
-  }
+                var formData = new FormData();
+                formData.append('screenshot', blob);
+                formData.append('url', location.href);
+                // await uploadArchivedMedia(accessToken, formData);
+                await uploadArchivedMedia(accessToken, formData);
+            });
+    } catch (err) {
+        console.log('Could not save Image', err);
+    }
 }
