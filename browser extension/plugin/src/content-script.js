72c2e662880c1ac253692dd9e9eb45e5473aea32
Denny <denny.george90@gmail.com>
2022-05-31 00:20:57 +0530

    fix(browser extension) : reliable injection of tweet control

--- browser extension/plugin/src/content-script.js
+++ browser extension/plugin/src/content-script.js
@@ -6,36 +6,29 @@ import { log } from './logger';
 
 log('Content Script Loaded');
 
-// setTimeout(async () => {
-//     processPage(location.href);
-// }, 5000);
-
-// async function initialize() {
-//     createTopBannerElement();
-
-//     const preference = await getPreferenceData();
-
-//     if (preference != undefined && preference.slurList != undefined) {
-//         updateSlurList(preference.slurList);
-//     }
-
-//     // process page
-// }
-
 function processPage(newUrl) {
-    let currentPage = current(newUrl);
-    log({ currentPage });
-    const { page } = currentPage;
-    const { getTimeline } = page;
+    var mainLoadedChecker = setInterval(() => {
+        console.log('tick');
+        const targetNode = document.getElementsByTagName('main')[0];
+        console.log({ targetNode });
+        if (targetNode) {
+            let currentPage = current(newUrl);
+            const { page } = currentPage;
+            const { getTimeline } = page;
 
-    if (getTimeline === undefined) {
-        log('Unknown State. Could not find Timeline');
-    } else {
-        let timeline = getTimeline();
-        log({ timeline });
-        transform.processNewlyAddedNodes(timeline.children);
-        setOnChangeListener(timeline, transform.processNewlyAddedNodes);
-    }
+            if (getTimeline === undefined) {
+                log('Unknown State. Could not find Timeline');
+            } else {
+                let timeline = getTimeline();
+                log({ timeline });
+                transform.processNewlyAddedNodes(timeline.children);
+                setOnChangeListener(timeline, transform.processNewlyAddedNodes);
+            }
+            clearInterval(mainLoadedChecker);
+        } else {
+            console.log('main section loaded');
+        }
+    }, 500);
 }
 
 /**
@@ -47,31 +40,15 @@ chrome.runtime.onMessage.addListener(function (request) {
     if (request.message === 'URL_CHANGED') {
         const newUrl = request.url;
         log('Url Changed', newUrl);
-        setTimeout(async () => {
-            processPage(location.href);
-        }, 2000);
+        processPage(location.href);
     }
 });
 
-const targetNode = document.getElementsByTagName('main')[0];
-const config = { attributes: true, childList: true, subtree: true };
-
-const callback = function (mutationsList) {
-    for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            log('A child node has been added or removed.');
-            const nodes = Array.from(mutation.addedNodes);
-            nodes.map((node) => {
-                if (node.tagName === 'SECTION') {
-                    log('Section Loaded', node);
-                    setTimeout(async () => {
-                        processPage(location.href);
-                    }, 500);
-                }
-            });
-        }
-    }
-};
-
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+window.addEventListener(
+    'load',
+    () => {
+        console.log('content loaded');
+        processPage(location.href);
+    },
+    false
+);
