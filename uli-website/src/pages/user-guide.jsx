4c6cbb1e860f71e5466b6d5f49a64d0f8134377b
Denny <denny.george90@gmail.com>
2022-05-30 15:56:53 +0530

    feat(uli-website): make user guide public

--- uli-website/src/pages/user-guide.jsx
+++ uli-website/src/pages/user-guide.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Box, Text, Heading } from "grommet";
-import { CTAHeadlineOne, SectionLableOne } from "../components/atoms/UliCore";
+import {
+  Colors,
+  CTAHeadlineOne,
+  SectionLableOne,
+} from "../components/atoms/UliCore";
 import AppShell from "../components/molecules/AppShell";
 
 const UserGuide = () => {
@@ -34,41 +38,69 @@ const UserGuide = () => {
             </Box>
           </Box>
 
-          <Box height={"4em"}></Box>
-          <Box alignSelf="center" gap={"small"} direction={"row-responsive"}>
+          <Box id={"conf"} height={"4em"}></Box>
+          <Box
+            alignSelf="center"
+            gap={"small"}
+            direction={"row-responsive"}
+            margin={{ bottom: "large" }}
+          >
             <Text style={{ fontSize: "2em" }}>Configuring Uli</Text>
           </Box>
           <Box direction="row-responsive" gap={"medium"}>
-            <Box width={"4em"}>
-              <img src={"/Slur Replacement Icon.png"}></img>
+            <Box width={"large"}>
+              <img src={"/configuration-screen.png"}></img>
             </Box>
-            <Box>
-              <Text weight={700}>Store Tweet Locally</Text>
-              <Text>
-                If you only want to store your images locally, select this. This
-                will ensure that your screenshots don't leave your device
-              </Text>
-              <Text weight={700}>Language</Text>
-              <Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </Text>
+            <Box gap={"medium"}>
+              <Box>
+                <Text id="conf-1" color={Colors.COLOR_ORANGE}>
+                  1. Store Tweets on Computer
+                </Text>
+                <Text>
+                  If you only want to store your images locally, select this.
+                  This will ensure that your screenshots don't leave your device
+                </Text>
+              </Box>
+              <Box>
+                <Text id="conf-2" color={Colors.COLOR_ORANGE}>
+                  2. Language
+                </Text>
+                <Text>
+                  Set the primary language for Uli. We currently support
+                  English, Hindi and Tamil.
+                </Text>
+              </Box>
 
-              <Text weight={700}>Your Email</Text>
-              <Text>
-                Ut consectetur molestie mauris vitae sagittis. Suspendisse
-                scelerisque velit sit amet ornare commodo.
-              </Text>
+              <Box>
+                <Text id="conf-3" color={Colors.COLOR_ORANGE}>
+                  3. Your Email Address
+                </Text>
+                <Text>
+                  We use this email to send you a copy of any archived tweet.
+                </Text>
+              </Box>
 
-              <Text weight={700}>Friends</Text>
-              <Text>
-                n laoreet est ac ligula luctus, eu mattis sem efficitur.
-              </Text>
+              <Box>
+                <Text id="conf-4" color={Colors.COLOR_ORANGE}>
+                  4. Friends
+                </Text>
+                <Text>
+                  When you use the "Invoke Network" feature, we use the email
+                  ids mentioned here to contact your friends. You can specify
+                  multiple email ids if you separate them by commas.
+                </Text>
+              </Box>
 
-              <Text weight={700}>Your Slur List</Text>
-              <Text>
-                Donec quis diam ipsum. Nam venenatis mi eleifend, convallis
-                tellus eget, tempus quam.
-              </Text>
+              <Box>
+                <Text id="conf-5" color={Colors.COLOR_ORANGE}>
+                  5. Your Slur List
+                </Text>
+                <Text>
+                  You can add words that you find offensive and want to hide
+                  from your timeline. You can specify multiple slurs if you
+                  separate them by commas.
+                </Text>
+              </Box>
             </Box>
           </Box>
           {/* add picture of preference screen with labels */}
@@ -115,6 +147,61 @@ const UserGuide = () => {
               of other festures suggested during our conversion can be accessed
               here: https://tinyurl.com/2p9bnefk
             </Text>
+
+            <Box>
+              <Text size={"xlarge"}>
+                How will you continue to maintain this tool?
+              </Text>
+
+              <Text>
+                This pilot project is supported by a grant from Omidyar Network
+                India. Given the experience of other similar projects, we
+                understand that projects such as these should be sustainable in
+                order to remain useful in the long run. With this in mind, we
+                aim to design the tool in such a way that it can be managed
+                affordably in the long run. If the pilot succeeds, we would
+                focus on long-term fundraising to keep this project running.
+              </Text>
+            </Box>
+
+            <Box>
+              <Text size={"xlarge"}>
+                What are your future plans with the archive feature?
+              </Text>
+
+              <Text>
+                We hope to create an anonymised public repository.of hatespeech
+                and harassment on social media targetting sexual and gender
+                minorities. We hope that this data base will support future
+                research on online violence and will also help activists,
+                lawyers and researchers in their advicacy efforts and build
+                discourse around online violence.
+              </Text>
+            </Box>
+
+            <Box>
+              <Text size={"xlarge"}>Why do you need my email address?</Text>
+
+              <Text>
+                We need your email address in order to send your archived tweets
+                to your email address. Your email is not used to correspond with
+                you regarding any Tattle or CIS events, promotions etc. It is
+                not shared with any third party as well.
+              </Text>
+            </Box>
+
+            <Box>
+              <Text size={"xlarge"}>What is this slur list? </Text>
+
+              <Text>
+                We crowdsourced a list of offensive words and phrases that
+                areused online. We used this list to scrape some of the content
+                off Twitter and build an inclusive dataset to train the machine
+                learning model. A smaller version of this list, containing slurs
+                that are commonly used was coded into the plugin to help with
+                the slur replacement feature.
+              </Text>
+            </Box>
           </Box>
         </Box>
       </Box>
