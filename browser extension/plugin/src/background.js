fb9da3db45c4748e3f3aeb3e20bf9178e9325b03
Denny <denny.george90@gmail.com>
2022-05-30 15:04:32 +0530

    feat(browser extension): send message from service worker upon url change

--- browser extension/plugin/src/background.js
+++ browser extension/plugin/src/background.js
@@ -1,6 +1,4 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-    // read changeInfo data and do something with it
-    // like send the new url to contentscripts.js
     if (changeInfo.url) {
         chrome.tabs.sendMessage(tabId, {
             message: 'URL_CHANGED',
