9f4d5c14ad5ab95c3c7c329fd4b74c124e181dd9
Sudeep Duggal <5505558+duggalsu@users.noreply.github.com>
2022-07-18 20:52:38 +0530

    Fixed lint errors. Removed unused-vars

--- browser-extension/plugin/src/ui-components/pages/Resources.jsx
+++ browser-extension/plugin/src/ui-components/pages/Resources.jsx
@@ -1,173 +1,175 @@
-import { Box, Heading, Text, Anchor, Button } from "grommet";
-import { useTranslation } from "react-i18next";
+import { Box, Heading, Text, Anchor /*, Button*/ } from 'grommet';
+// import { useTranslation } from "react-i18next";
 
 export function Resources() {
-  const { t, i18n } = useTranslation();
-  return (
-    <Box width={"medium"}>
-      <Heading level={3}>English</Heading>
+    // const { t, i18n } = useTranslation();
+    return (
+        <Box width={'medium'}>
+            <Heading level={3}>English</Heading>
 
-      <Anchor
-        href={"https://iheartmob.org/resources"}
-        label={"Resources related to Online Harassment:"}
-        target={"_blank"}
-      />
-      <Anchor
-        href={"https://iheartmob.org/pages/twitter-safety-guide"}
-        label={"Twitter Guidelines Simplified"}
-        target={"_blank"}
-      />
+            <Anchor
+                href={'https://iheartmob.org/resources'}
+                label={'Resources related to Online Harassment:'}
+                target={'_blank'}
+            />
+            <Anchor
+                href={'https://iheartmob.org/pages/twitter-safety-guide'}
+                label={'Twitter Guidelines Simplified'}
+                target={'_blank'}
+            />
 
-      <Heading level={4}>Understand Online Harassment:</Heading>
-      <Anchor
-        href={
-          "https://vikaspedia.in/education/digital-litercy/information-security/being-safe-online-1/cyber-harassment"
-        }
-        label={"Online Harassment in the Indian Context: "}
-        target={"_blank"}
-      />
-      <Anchor
-        href={
-          "https://onlineharassmentfieldmanual.pen.org/defining-online-harassment-a-glossary-of-terms/"
-        }
-        label={"Defining “Online Abuse”: A Glossary of Terms: "}
-        target={"_blank"}
-      />
-      <Anchor
-        href={
-          "https://genderit.org/resources/13-manifestations-gender-based-violence-using-technology"
-        }
-        label={"Visual guide to online Harassment: "}
-        target={"_blank"}
-      />
-      <Anchor
-        href={
-          "https://theswaddle.com/know-your-rights-protection-against-online-harassment/"
-        }
-        label={"Know your Rights"}
-        target={"_blank"}
-      />
-      <Anchor
-        href={
-          "https://legodesk.com/blog/knowledge-base/online-harassment-laws-in-india-your-rights-remedies/ "
-        }
-        target={"_blank"}
-        label={"Laws Related to Online Harassment in India"}
-      />
+            <Heading level={4}>Understand Online Harassment:</Heading>
+            <Anchor
+                href={
+                    'https://vikaspedia.in/education/digital-litercy/information-security/being-safe-online-1/cyber-harassment'
+                }
+                label={'Online Harassment in the Indian Context: '}
+                target={'_blank'}
+            />
+            <Anchor
+                href={
+                    'https://onlineharassmentfieldmanual.pen.org/defining-online-harassment-a-glossary-of-terms/'
+                }
+                label={'Defining “Online Abuse”: A Glossary of Terms: '}
+                target={'_blank'}
+            />
+            <Anchor
+                href={
+                    'https://genderit.org/resources/13-manifestations-gender-based-violence-using-technology'
+                }
+                label={'Visual guide to online Harassment: '}
+                target={'_blank'}
+            />
+            <Anchor
+                href={
+                    'https://theswaddle.com/know-your-rights-protection-against-online-harassment/'
+                }
+                label={'Know your Rights'}
+                target={'_blank'}
+            />
+            <Anchor
+                href={
+                    'https://legodesk.com/blog/knowledge-base/online-harassment-laws-in-india-your-rights-remedies/ '
+                }
+                target={'_blank'}
+                label={'Laws Related to Online Harassment in India'}
+            />
 
-      <Heading level={4}>Respond to online harassment:</Heading>
-      <Anchor
-        href={"https://onlineharassmentfieldmanual.pen.org/respond/"}
-        label={"Online Harassment Field Manual: "}
-        target={"_blank"}
-      />
-      <Anchor
-        href={"https://onlinesafety.feministfrequency.com/en/"}
-        target={"_blank"}
-        label={"Speak Up & Stay Safe"}
-      />
-      <Anchor
-        href={"https://cybercivilrights.org/twitter/"}
-        target={"_blank"}
-        label="Reporting of Non Consensual Sharing of Intimate Images on Twitter"
-      />
-      <Anchor
-        href={
-          "https://cybercivilrights.org/online-removal/#1626116897126-490fcf07-3350"
-        }
-        target={"_blank"}
-        label="Reporting on Other platforms"
-      />
+            <Heading level={4}>Respond to online harassment:</Heading>
+            <Anchor
+                href={'https://onlineharassmentfieldmanual.pen.org/respond/'}
+                label={'Online Harassment Field Manual: '}
+                target={'_blank'}
+            />
+            <Anchor
+                href={'https://onlinesafety.feministfrequency.com/en/'}
+                target={'_blank'}
+                label={'Speak Up & Stay Safe'}
+            />
+            <Anchor
+                href={'https://cybercivilrights.org/twitter/'}
+                target={'_blank'}
+                label="Reporting of Non Consensual Sharing of Intimate Images on Twitter"
+            />
+            <Anchor
+                href={
+                    'https://cybercivilrights.org/online-removal/#1626116897126-490fcf07-3350'
+                }
+                target={'_blank'}
+                label="Reporting on Other platforms"
+            />
 
-      <Anchor
-        href={"https://digitalfirstaid.org/en/"}
-        label={"An interactive guide to deal with online Harassment"}
-        target={"_blank"}
-      />
+            <Anchor
+                href={'https://digitalfirstaid.org/en/'}
+                label={'An interactive guide to deal with online Harassment'}
+                target={'_blank'}
+            />
 
-      <Heading level={4}>Support Networks</Heading>
-      <Anchor
-        href={"https://twitter.com/TeamSaath"}
-        label={"Team Saath"}
-        target={"_blank"}
-      />
-      <Anchor
-        href={"http://www.cybervictims.org/"}
-        label={"Cyber Victims"}
-        target={"_blank"}
-      />
+            <Heading level={4}>Support Networks</Heading>
+            <Anchor
+                href={'https://twitter.com/TeamSaath'}
+                label={'Team Saath'}
+                target={'_blank'}
+            />
+            <Anchor
+                href={'https://www.cybervictims.org/'}
+                label={'Cyber Victims'}
+                target={'_blank'}
+            />
 
-      <Heading level={3}>Hindi</Heading>
-      <Heading level={4}>ट्विटर सहायता केंद्र:</Heading>
-      <Anchor
-        href={"https://help.twitter.com/hi/rules-and-policies/twitter-rules"}
-        label={"ट्विटर के नियम:"}
-        target={"_blank"}
-      />
-      <Anchor
-        href={"https://help.twitter.com/hi/resources/a-safer-twitter"}
-        label={"एक सुरक्षित ट्विटर"}
-        target={"_blank"}
-      />
-      <Anchor
-        href={
-          "https://nyaaya.org/topic/%E0%A4%91%E0%A4%A8%E0%A4%B2%E0%A4%BE%E0%A4%87%E0%A4%A8-%E0%A4%A6%E0%A5%81%E0%A4%B0%E0%A5%81%E0%A4%AA%E0%A4%AF%E0%A5%8B%E0%A4%97/?lang=hi"
-        }
-        label={"ऑनलाइन उत्पीड़न को समझें"}
-        target={"_blank"}
-      />
-      <Anchor
-        href={
-          "https://nyaaya.org/%E0%A4%91%E0%A4%A8%E0%A4%B2%E0%A4%BE%E0%A4%87%E0%A4%A8-%E0%A4%89%E0%A4%A4%E0%A5%8D%E0%A4%AA%E0%A5%80%E0%A4%A1%E0%A4%BC%E0%A4%A8-%E0%A4%94%E0%A4%B0-%E0%A4%B9%E0%A4%BF%E0%A4%82%E0%A4%B8%E0%A4%BE/?lang=hi"
-        }
-        label={"ऑनलाइन उत्पीड़न और हिंसा"}
-        target={"_blank"}
-      />
+            <Heading level={3}>Hindi</Heading>
+            <Heading level={4}>ट्विटर सहायता केंद्र:</Heading>
+            <Anchor
+                href={
+                    'https://help.twitter.com/hi/rules-and-policies/twitter-rules'
+                }
+                label={'ट्विटर के नियम:'}
+                target={'_blank'}
+            />
+            <Anchor
+                href={'https://help.twitter.com/hi/resources/a-safer-twitter'}
+                label={'एक सुरक्षित ट्विटर'}
+                target={'_blank'}
+            />
+            <Anchor
+                href={
+                    'https://nyaaya.org/topic/%E0%A4%91%E0%A4%A8%E0%A4%B2%E0%A4%BE%E0%A4%87%E0%A4%A8-%E0%A4%A6%E0%A5%81%E0%A4%B0%E0%A5%81%E0%A4%AA%E0%A4%AF%E0%A5%8B%E0%A4%97/?lang=hi'
+                }
+                label={'ऑनलाइन उत्पीड़न को समझें'}
+                target={'_blank'}
+            />
+            <Anchor
+                href={
+                    'https://nyaaya.org/%E0%A4%91%E0%A4%A8%E0%A4%B2%E0%A4%BE%E0%A4%87%E0%A4%A8-%E0%A4%89%E0%A4%A4%E0%A5%8D%E0%A4%AA%E0%A5%80%E0%A4%A1%E0%A4%BC%E0%A4%A8-%E0%A4%94%E0%A4%B0-%E0%A4%B9%E0%A4%BF%E0%A4%82%E0%A4%B8%E0%A4%BE/?lang=hi'
+                }
+                label={'ऑनलाइन उत्पीड़न और हिंसा'}
+                target={'_blank'}
+            />
 
-      <Anchor
-        href={
-          "https://www.infosecawareness.in/concept/online-predators?lang=hi"
-        }
-        label={"ऑनलाइन उत्पीड़न का मुकाबला कैसे करें"}
-        target={"_blank"}
-      />
+            <Anchor
+                href={
+                    'https://www.infosecawareness.in/concept/online-predators?lang=hi'
+                }
+                label={'ऑनलाइन उत्पीड़न का मुकाबला कैसे करें'}
+                target={'_blank'}
+            />
 
-      <Anchor
-        href={
-          "https://gijn.org/2021/07/30/journalists-online-harassment-hindi/"
-        }
-        label={"पत्रकारों के लिए"}
-        target={"_blank"}
-      />
+            <Anchor
+                href={
+                    'https://gijn.org/2021/07/30/journalists-online-harassment-hindi/'
+                }
+                label={'पत्रकारों के लिए'}
+                target={'_blank'}
+            />
 
-      <Heading level={4}>
-        ऑनलाइन उत्पीड़न से निपटने वाले संगठनों के लिंक:
-      </Heading>
-      <Anchor
-        href={"https://twitter.com/TeamSaath"}
-        label={"टीम साथ"}
-        target={"_blank"}
-      />
-      <Anchor
-        href={"http://www.cybervictims.org/"}
-        label={"साइबर विक्टिम्स"}
-        target={"_blank"}
-      />
+            <Heading level={4}>
+                ऑनलाइन उत्पीड़न से निपटने वाले संगठनों के लिंक:
+            </Heading>
+            <Anchor
+                href={'https://twitter.com/TeamSaath'}
+                label={'टीम साथ'}
+                target={'_blank'}
+            />
+            <Anchor
+                href={'https://www.cybervictims.org/'}
+                label={'साइबर विक्टिम्स'}
+                target={'_blank'}
+            />
 
-      <Heading level={3}>Tamil (under construction)</Heading>
-      <Anchor
-        href={
-          "https://tam.4meahc.com/what-do-if-youve-been-threatened-online-01683#menu-4"
-        }
-        label={"1"}
-        target={"_blank"}
-      />
-      <Text>
-        Support Networks: Dhwani 24*7 toll free helpline by PCVC: 044-43111143 /
-        Whatsapp chat support: 9840888882
-      </Text>
+            <Heading level={3}>Tamil (under construction)</Heading>
+            <Anchor
+                href={
+                    'https://tam.4meahc.com/what-do-if-youve-been-threatened-online-01683#menu-4'
+                }
+                label={'1'}
+                target={'_blank'}
+            />
+            <Text>
+                Support Networks: Dhwani 24*7 toll free helpline by PCVC:
+                044-43111143 / Whatsapp chat support: 9840888882
+            </Text>
 
-      {/* <Button
+            {/* <Button
         label={"test"}
         onClick={() => {
           console.log("clicked");
@@ -179,6 +181,6 @@ export function Resources() {
           );
         }}
       /> */}
-    </Box>
-  );
+        </Box>
+    );
 }
