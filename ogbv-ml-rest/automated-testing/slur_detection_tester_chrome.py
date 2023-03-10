e6f2e5d7d9d55fcdf79dd235ab243fb7d882a28a
Bhargav Dave <58621145+Bhargav-Dave@users.noreply.github.com>
2023-03-10 10:25:27 +0530

    fix: changed e2e testing files to match new changes (#231)

--- ogbv-ml-rest/automated-testing/slur_detection_tester_chrome.py
+++ ogbv-ml-rest/automated-testing/slur_detection_tester_chrome.py
@@ -13,18 +13,18 @@ from selenium.webdriver.remote.webelement import WebElement
 # Configure the necessary command-line option
 options = webdriver.ChromeOptions()
 # Note that you will need to download the build of the extension and put the path to the dist folder 
-options.add_argument(r'--load-extension=path\to\dist_folder_of_extension')
+options.add_argument(r'--load-extension=path/to/dist/folder')
 
 # installing chromedriver 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options = options)
 sleep(2)
 # add a hardcoded url leading to a particular tweet which you are using for testing 
-driver.get('your url here')
-sleep(5)
+driver.get('https://twitter.com/jackantonoff/status/1579311659742416896')
+sleep(7)
 # Check if the extension worked and log the result. 
 try:
     # logic to check if the slur is replaced
-    ele = driver.find_element('xpath','//span[contains(text(),"▓")]')
+    ele = driver.find_element('xpath','//div[contains(text(),"▓")]')
     print('Success! :-)')
     # if no such span is present where the character - ▓ - is not present then the Failure message will show up
     # Note that this will return success if the tweet by default contains the replacement character 
