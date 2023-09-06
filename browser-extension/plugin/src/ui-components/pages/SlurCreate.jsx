8c9b5666473a1a4421688f3e1cb11efcfaf68ad3
Aatman Vaidya <aatmanvaidya@gmail.com>
2023-09-06 13:26:34 +0530

    adding more UI in ADD

--- browser-extension/plugin/src/ui-components/pages/SlurCreate.jsx
+++ browser-extension/plugin/src/ui-components/pages/SlurCreate.jsx
@@ -1,49 +1,105 @@
 import { useState } from 'react';
-import { Box, Button, Heading, Form, FormField, TextInput } from 'grommet';
+import {
+    Box,
+    Button,
+    Form,
+    FormField,
+    Heading,
+    RadioButtonGroup,
+    Select,
+    TextArea,
+    TextInput,
+    Anchor
+} from 'grommet';
+import { useNavigate } from 'react-router-dom';
 
-// const genderOptions = ['a', 'b', 'c']
-// const religionOptions = ['Option 1', 'Option 2', 'Option 3']
-// const sexualOrientationOptions = ['Option A', 'Option B', 'Option C']
+const genderOptions = ['a', 'b', 'c'];
+const religionOptions = ['Option 1', 'Option 2', 'Option 3'];
+const sexualOrientationOptions = ['Option A', 'Option B', 'Option C'];
+const appropriatedOptions = [true, false];
 
 export function SlurCreate() {
-    // const [formData, setFormData] = useState({
-    //     label: '',
-    //     labelMeaning: '',
-    //     gender: '',
-    //     religion: '',
-    //     sexualOrientation: '',
-    //     appropriated: 'false',
-    //     appropriationContext: '',
-    // })
-    const [value, setValue] = useState(undefined);
+    const initialFormData = {
+        label: '',
+        labelMeaning: '',
+        gender: '',
+        religion: '',
+        sexualOrientation: '',
+        appropriated: false,
+        appropriationContext: ''
+    };
+    const [formData, setFormData] = useState(initialFormData);
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate('/slur');
+    };
 
     const handleSubmit = (value) => {
         console.log(value);
-        // console.log(formData);
+        console.log(formData);
+    };
+    const handleReset = () => {
+        setFormData(initialFormData);
     };
 
     return (
         <Box>
-            <Heading level={3} weight={'bold'}>
+            <Anchor onClick={handleGoBack}>Go Back</Anchor>
+            <Heading level={3} weight={'bold'} alignSelf="center">
                 Add Slur
             </Heading>
             <Form
+                value={formData}
+                onChange={(nextValue) => setFormData(nextValue)}
                 onSubmit={handleSubmit}
-                value={value}
-                onChange={(nextValue, { touched }) => {
-                    console.log('Change', nextValue, touched);
-                    setValue(nextValue);
-                }}
             >
-                <FormField name="label" label="label">
-                    <TextInput
-                        name="label"
-                        // value={formData.label}
-                        // onChange={(event) => setFormData({ ...formData, label: event.target.value })}
-                        // required
+                <FormField name="label" label="Label" required>
+                    <TextInput name="label" />
+                </FormField>
+
+                <FormField name="labelMeaning" label="Label Meaning" required>
+                    <TextArea name="labelMeaning" />
+                </FormField>
+
+                <FormField name="gender" label="Gender" required>
+                    <RadioButtonGroup name="gender" options={genderOptions} />
+                </FormField>
+
+                <FormField name="religion" label="Religion" required>
+                    <Select name="religion" options={religionOptions} />
+                </FormField>
+
+                <FormField
+                    name="sexualOrientation"
+                    label="Sexual Orientation"
+                    required
+                >
+                    <Select
+                        name="sexualOrientation"
+                        options={sexualOrientationOptions}
                     />
                 </FormField>
-                <Button type="submit" primary label="Submit" />
+
+                <FormField name="appropriated" label="Appropriated" required>
+                    <RadioButtonGroup
+                        name="appropriated"
+                        options={appropriatedOptions}
+                        defaultValue={false}
+                    />
+                </FormField>
+
+                <FormField
+                    name="appropriationContext"
+                    label="Appropriation Context"
+                    required
+                >
+                    <TextInput name="appropriationContext" />
+                </FormField>
+
+                <Box direction="row" gap="medium">
+                    <Button type="submit" primary label="Submit" />
+                    <Button type="reset" label="Reset" onClick={handleReset} />
+                </Box>
             </Form>
         </Box>
     );
