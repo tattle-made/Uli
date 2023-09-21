49a1b013a926b9c6c0d3fcd4c1c8f2c288797572
Aurora <5505558+duggalsu@users.noreply.github.com>
2023-09-21 13:03:05 +0530

    - Updated readme - Updated firefox automated testing

--- browser-extension/automated-testing/firefox/slur_detection_tester_firefox.py
+++ browser-extension/automated-testing/firefox/slur_detection_tester_firefox.py
@@ -6,17 +6,55 @@ from time import sleep
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
-# TODO: Add path to dist folder
-driver.install_addon('/path/to/extension/dist/', temporary=True)
+# TODO: Add path to dist folder without trailing separator ('/')
+driver.install_addon('path/to/dist', temporary=True)
+sleep(5)
+
+# workflow
+driver.get('about:debugging#/runtime/this-firefox')
+sleep(2)
+uli_span = driver.find_element('xpath','//span[contains(text(),"uli")]')
+uli_span_parent = uli_span.find_element('xpath', './..')
+ids = uli_span_parent.find_elements(By.TAG_NAME, 'dd')
+# get Internal UUID instead of extension ID for automated testing
+internal_UUID = str(ids[2].text)
+sleep(2)
+print(internal_UUID)
+
+# going to the options page
+url = "moz-extension://" + internal_UUID + "/options.html"
+driver.get(url)
+sleep(5)
+
+# Activate Uli
+activate_button = driver.find_element('id', 'app_btn_activate')
+activate_button.click()
+
+# Confirm page reload on activation
+WebDriverWait(driver, 10).until(EC.alert_is_present())
+driver.switch_to.alert.accept()
+
+# Reload page manually for automated handling - '/preferences' page load fails
+url = "moz-extension://" + internal_UUID + "/options.html"
+driver.get(url)
+
+# Save settings
+save_button = driver.find_element('id', 'app_btn_save') #saving the settings
+save_button.click()
+# need sleep() here - else page tries to migrate to '/preferences' which fails
 sleep(5)
 
 # add a hardcoded url leading to a particular tweet which you are using for testing 
 # a test url is already given
 driver.get('https://twitter.com/jackantonoff/status/1579311659742416896')
-sleep(5)
+sleep(20)
 
 # Check if the extension worked and log the result.
 try:
