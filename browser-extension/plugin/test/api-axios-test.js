2a0df4618a506cd4b9446cfc601d8c43a9daf55b
Aurora <5505558+duggalsu@users.noreply.github.com>
2023-11-01 16:24:33 +0530

    Update axios (#462)
    * Update axios
    - Updated axios version
    - Pinned auto-added buffer package required by parcel
    
    * - Rebuilt package lock file
    
    * Add Api axios tests
    - Added Api tests other than slur crowdsource

--- browser-extension/plugin/test/api-axios-test.js
+++ browser-extension/plugin/test/api-axios-test.js
@@ -2,8 +2,46 @@ const axios = require('axios');
 
 const API_URL = 'http://localhost:3000';
 
+/**
+ * TODO: Currently not added from Api.js 
+ * - login()
+ * - getPreferenceForUser() - not used anywhere
+ * - uploadArchivedMedia()
+ * - getArchive()
+ * - invokeNetwork()
+*/
+
 async function registerNewUser() {
     return axios.get(`${API_URL}/auth/register`);
+}
+
+async function getPreferenceForUser(accessToken) {
+    const result = await axios.get(`${API_URL}/preference`, {
+        headers: {
+            Authorization: `token ${accessToken}`
+        }
+    });
+    return result.data.preference;
+}
+
+async function savePreference(accessToken, preference) {
+    return axios.post(`${API_URL}/preference`, preference, {
+        headers: {
+            Authorization: `token ${accessToken}`
+        }
+    });
+}
+
+async function resetAccount(accessToken) {
+    return axios.post(
+        `${API_URL}/reset`,
+        {},
+        {
+            headers: {
+                Authorization: `token ${accessToken}`
+            }
+        }
+    );
 }
 
 async function createSlurAndCategory(accessToken, slurData) {
@@ -41,6 +79,9 @@ async function getSlurAndCategory(accessToken) {
 
 module.exports = {
     registerNewUser,
+    getPreferenceForUser,
+    savePreference,
+    resetAccount,
     createSlurAndCategory,
     updateSlurAndCategory,
     deleteSlurAndCategory, 
