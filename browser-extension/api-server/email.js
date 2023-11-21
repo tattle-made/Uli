958195abdf22f7682a4be2dbc0588430cf3b6af9
Aurora <5505558+duggalsu@users.noreply.github.com>
2023-11-21 10:25:07 +0530

    Update aws and multer (#474)
    * Update axios
    - Updated axios version
    - Pinned auto-added buffer package required by parcel
    
    # Conflicts:
    #	browser-extension/plugin/package-lock.json
    #	browser-extension/plugin/package.json
    
    * - Recreated package lock file
    
    # Conflicts:
    #	browser-extension/plugin/package-lock.json
    
    * Update aws-sdk and multer-s3
    - Updated AWS SDK for JS packages
    - Updated multer-s3 package
    - Migrated AWS code to use current package version
    
    * Update AWS SDK modules
    - Updated aws-sdk client-s3
    - Updated aws-sdk client-sesv2

--- browser-extension/api-server/email.js
+++ browser-extension/api-server/email.js
@@ -1,17 +1,6 @@
-const AWS = require("aws-sdk");
-
-AWS.config.update({
-  region: "ap-south-1",
-  credentials: {
-    accessKeyId: process.env.SERVICE_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.SERVICE_AWS_SECRET_ACCESS_KEY,
-  },
-});
-
-try {
-} catch (error) {
-  console.log("Could not connect to AWS SES");
-}
+const {
+  SES
+} = require("@aws-sdk/client-sesv2");
 
 const constructParamsObject = ({ subject, body, receiver }) => {
   const params = {
@@ -38,7 +27,19 @@ const constructParamsObject = ({ subject, body, receiver }) => {
 };
 
 const sendEmail = async ({ subject, body, receiver }) => {
-  const sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+  const sendPromise = new SES({
+    // The transformation for apiVersion is not implemented.
+    // Refer to UPGRADING.md on aws-sdk-js-v3 for changes needed.
+    // Please create/upvote feature request on aws-sdk-js-codemod for apiVersion.
+    apiVersion: "2010-12-01",
+
+    region: "ap-south-1",
+
+    credentials: {
+      accessKeyId: process.env.SERVICE_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.SERVICE_AWS_SECRET_ACCESS_KEY,
+    }
+  })
     .sendEmail(constructParamsObject({ subject, body, receiver }))
     .promise();
   try {
