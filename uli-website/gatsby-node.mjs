acd647d9a0ae617cbdfcca1790ed6aa4c401856d
Aurora <5505558+duggalsu@users.noreply.github.com>
2023-11-22 18:52:17 +0530

    Fix gatsby slug issue - Added absoluteFilePath to resolve base url concat issue

--- uli-website/gatsby-node.mjs
+++ uli-website/gatsby-node.mjs
@@ -1,12 +1,15 @@
 import { createFilePath } from "gatsby-source-filesystem"
+import config from "./gatsby-config.js"
 
 export const onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === 'Mdx') {
+    const relativeFilePath = createFilePath({ node, getNode })
+    const absoluteFilePath = `${config.siteMetadata.siteUrl}${relativeFilePath}`
     createNodeField({
       node,
       name: 'slug',
-      value: createFilePath({ node, getNode })
+      value: `${relativeFilePath}`
     });
   }
 };
