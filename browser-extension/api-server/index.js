58320d098e708a312b290409a3d0ea8ebbaac9ad
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-07-22 14:33:23 +0530

    chore: moving cors middleware to top

--- browser-extension/api-server/index.js
+++ browser-extension/api-server/index.js
@@ -6,25 +6,22 @@ app.disable("x-powered-by");
 const port = 3000;
 const cors = require("cors");
 const { authentication } = require("./middlewares");
-app.use(authentication);
-
 
 app.use(cors());
+app.use(authentication);
 app.use(express.json());
 app.options("*", cors());
 
-
-app.use("/" , require("./routes/slur.js")) ; 
-app.use("/" , require('./routes/archive.js')) ; 
-app.use("/" , require('./routes/reset.js')) ; 
-app.use("/" , require('./routes/preference.js')) ; 
-app.use("/" , require('./routes/predict.js')) ; 
-app.use("/" , require('./routes/post.js')) ; 
-app.use("/" , require('./routes/auth.js')) ; 
-app.use("/" , require('./routes/feedback.js')) ; 
+app.use("/", require("./routes/slur.js"));
+app.use("/", require("./routes/archive.js"));
+app.use("/", require("./routes/reset.js"));
+app.use("/", require("./routes/preference.js"));
+app.use("/", require("./routes/predict.js"));
+app.use("/", require("./routes/post.js"));
+app.use("/", require("./routes/auth.js"));
+app.use("/", require("./routes/feedback.js"));
 
 console.log(`Environment : ${process.env.NODE_ENV}`);
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
