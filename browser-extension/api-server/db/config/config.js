3663a1052af5ecff9776fd4a72bd59a9f5021a3a
maanasb01 <maanasb01@gmail.com>
2024-09-16 16:48:35 +0530

    feat: db changed to postgres

--- browser-extension/api-server/db/config/config.js
+++ browser-extension/api-server/db/config/config.js
@@ -4,20 +4,20 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: "uli_dev",
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: "postgres",
   },
   test: {
     username: "root",
     password: null,
     database: "uli_test",
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: "uli_prod_unicode",
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: "postgres",
   },
 };
