36109e7d411afc5f40152b1c6a2aad6723a08f8c
maanasb01 <maanasb01@gmail.com>
2024-11-18 18:27:39 +0530

    generate auth code

---
+++ uli_community/lib/uli_community_web/controllers/user_session_html.ex
@@ -0,0 +1,5 @@
+defmodule UliCommunityWeb.UserSessionHTML do
+  use UliCommunityWeb, :html
+
+  embed_templates "user_session_html/*"
+end
---
+++ uli_community/lib/uli_community_web/controllers/user_settings_html.ex
@@ -0,0 +1,5 @@
+defmodule UliCommunityWeb.UserSettingsHTML do
+  use UliCommunityWeb, :html
+
+  embed_templates "user_settings_html/*"
+end
