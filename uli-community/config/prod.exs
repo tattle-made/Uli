48e9388d30b7d158209f6a5a7cb2eaf7eb2ecfe4
Denny <denny.george90@gmail.com>
2025-01-27 12:07:48 +0530

    fix: set prod server endpoint to true

--- uli-community/config/prod.exs
+++ uli-community/config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :uli_community, UliCommunityWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: UliCommunity.Finch
