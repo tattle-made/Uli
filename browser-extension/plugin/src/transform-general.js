a71c45665c039401d538efb0869cc42543ad1390
Aatman Vaidya <aatmanvaidya@gmail.com>
2025-02-06 17:48:26 +0530

    fix: improvements and bug fixes in hover metadata feature (#677)
    * fix: word breaking issue
    
    * fix: logic working on non-eng text and multi phrase words
    
    * chore: removing extra code
    
    * revert: to old working code
    
    * chore: sort target words
    
    * chore: wip hover reach element code
    
    * feat: basic tooltip rendering using react componnet
    
    * style: hover ui change
    
    * fix: google UI break
    
    * chore: search working on multi-phrase words
    
    * fix: delete and update metadata
    
    * revert: old logic to locate slurs
    
    * fix: fetching metadata from json object
    
    * chore: hover UI component changes
    
    * style: hover ui element
    
    * style: font change in hover ui
    
    * chore: add min-width to hover box, add vertical gap in categories
    
    * style: font change in hover ui
    
    * chore: manifest version update
    
    ---------
    
    Co-authored-by: maanasb01 <maanasb01@gmail.com>

--- browser-extension/plugin/src/transform-general.js
+++ browser-extension/plugin/src/transform-general.js
@@ -2,6 +2,8 @@ import { replaceSlur } from './slur-replace';
 import { log } from './logger';
 import repository from './repository';
 const { getPreferenceData } = repository;
+import { createRoot } from 'react-dom/client';
+import HoverSlurMetadata from './ui-components/atoms/HoverSlurMetadata';
 
 // Traverse dom nodes to find leaf node that are text nodes and process
 function bft(node) {
@@ -49,19 +51,6 @@ function setCaretPosition(element, offset) {
     sel.addRange(range);
 }
 
-const processNewlyAddedNodesGeneral2 = function (firstBody, jsonData) {
-    let targetWords = [];
-    jsonData.forEach((slur) => {
-        const slurWord = Object.keys(slur)[0];
-        targetWords.push(slurWord);
-        // targetWords.push(slurWord.charAt(0).toUpperCase() + slurWord.slice(1));
-    });
-    let uliStore = [];
-    getAllTextNodes(firstBody, uliStore);
-    abc = locateSlur(uliStore, targetWords);
-    addMetaData(targetWords, jsonData);
-};
-
 const processNewlyAddedNodesGeneral = function (firstBody) {
     log('processing new nodes');
     const config = { attributes: true, childList: true, subtree: true };
@@ -85,6 +74,16 @@ const processNewlyAddedNodesGeneral = function (firstBody) {
     observer.observe(firstBody, config);
 };
 
+const processNewlyAddedNodesGeneral2 = function (firstBody, jsonData) {
+    let targetWords = jsonData.map(slur => Object.keys(slur)[0]);
+    targetWords.sort((a, b) => b.length - a.length);
+
+    let uliStore = [];
+    getAllTextNodes(firstBody, uliStore);
+    locateSlur(uliStore, targetWords);
+    addMetaData(targetWords, jsonData);
+};
+
 function checkFalseTextNode(text, actualLengthOfText) {
     let totalNewlineAndWhitespaces = 0;
     for (let i = 0; i < text.length; i++) {
@@ -95,7 +94,6 @@ function checkFalseTextNode(text, actualLengthOfText) {
     return totalNewlineAndWhitespaces === actualLengthOfText;
 }
 
-// Function to recursively get all text nodes for a given node
 function getAllTextNodes(node, uliStore) {
     if (node.nodeType === 3) {
         if (!checkFalseTextNode(node.data, node.length)) {
@@ -125,178 +123,81 @@ function findPositions(word, text) {
 }
 
 function locateSlur(uliStore, targetWords) {
-    let n = uliStore.length;
-
-    for (let i = 0; i < n; i++) {
-        let store = uliStore[i];
-        let parentNode = store.parent;
+    uliStore.forEach(store => {
         let textnode = store.node;
-        let text = store.node.textContent;
         let tempParent = document.createElement('span');
-        tempParent.textContent = text;
-        let slurs = [];
+        tempParent.textContent = textnode.textContent;
         let slurPresentInTempParent = false;
-        targetWords.forEach((targetWord) => {
-            let slurWord = targetWord;
-            let pos = findPositions(slurWord, text);
-            if (Object.keys(pos).length !== 0) {
-                slurs.push(pos);
-            }
-
-            if (tempParent.innerHTML.includes(targetWord)) {
-                const className = `icon-container-${targetWord}`;
-                const slurClass = `slur-container-${targetWord}`;
-
-                // if (!tempParent.innerHTML.includes(`class="${slurClass}"`)) {
-                const parts = tempParent.innerHTML.split(targetWord);
-                const replacedHTML = parts.join(
-                    `<span class="${slurClass}"><span class="slur">${targetWord}</span></span>`
-                );
-                tempParent.innerHTML = replacedHTML;
+        targetWords.forEach(targetWord => {
+            const sanitizedTargetWord = targetWord.replace(/\s+/g, '-');
+            const slurClass = `slur-container-${sanitizedTargetWord}`;
+            
+            const escapedTargetWord = targetWord.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            // regex for multi-word and single-word phrases
+            const regex = new RegExp(`(^|\\s|[.,!?])(${escapedTargetWord})(?=\\s|$|[.,!?])`, 'giu');
+            
+            if (regex.test(tempParent.textContent)) {
+                tempParent.innerHTML = tempParent.innerHTML.replace(regex, (match, prefix, word) => {
+                    return `${prefix}<span class="${slurClass}"><span class="slur">${word}</span></span>`;
+                });
                 slurPresentInTempParent = true;
-                // }
             }
         });
-        uliStore[i].nodeToParent = tempParent;
-        uliStore[i].slurs = slurs;
 
-        //O(1) complexity
         if (slurPresentInTempParent) {
             textnode.replaceWith(tempParent);
         }
-
-        // console.log("TEMPParent: ",tempParent)
-    }
-    return uliStore;
+    });
 }
 
 function addMetaData(targetWords, jsonData) {
     targetWords.forEach((targetWord) => {
-        const className = `slur-container-${targetWord}`;
+        const sanitizedTargetWord = targetWord.replace(/\s+/g, '-');
+        const className = `slur-container-${sanitizedTargetWord}`;
         const elements = Array.from(document.querySelectorAll(`.${className}`));
-        // console.log("ELEMENTS are: ",elements)
+
         elements.forEach((element) => {
-            // console.log("ELements InnerHTML:",element.innerHTML)
+            const slur = element.querySelector('.slur');
+            if (!slur) return;
 
-            // element.innerHTML = element.innerHTML.replace(/<img[^>]*>/g, '')
-
-            let sup = document.createElement('span');
-            let img = document.createElement('img');
-            img.style.height = '0.6em';
-            img.style.width = '0.6em';
-            img.style.border = '1px solid black';
-            img.style.cursor = 'pointer';
-            img.style.marginBottom = '0.4em';
-
-            img.src =
-                'https://raw.githubusercontent.com/tattle-made/Uli/main/uli-website/src/images/favicon-32x32.png';
-            img.alt = 'altText';
-
-            let span = document.createElement('span');
-            span.style.display = 'none';
-            span.style.position = 'absolute';
-            span.style.marginLeft = '2px';
-            span.style.marginTop = '2px';
-            span.style.backgroundColor = 'antiquewhite';
-            span.style.border = '1px solid black';
-            span.style.borderRadius = '12px';
-            span.style.padding = '2px 6px';
-            span.style.width = '16rem';
-            span.style.textAlign = 'justify';
-            span.style.fontWeight = 'lighter';
-            span.style.color = 'black';
-            span.style.zIndex = '1000000000'; // This ensures it appears above other elements
-            span.style.fontSize = '14px';
-            span.style.textDecoration = 'none';
-            span.style.fontStyle = 'normal';
-
-            span.innerHTML = `${targetWord} is an offensive word`;
-
-            jsonData.forEach((slur) => {
-                const slurWord = Object.keys(slur)[0];
-                if (slurWord.toLowerCase() === targetWord.toLowerCase()) {
-                    const slurDetails = slur[slurWord];
-                    let levelOfSeverity = slurDetails['Level of Severity'];
-                    let casual = slurDetails['Casual'];
-                    let approapriated = slurDetails['Appropriated'];
-                    let reason =
-                        slurDetails[
-                            'If, Appropriated, Is it by Community or Others?'
-                        ];
-                    let problematic = slurDetails['What Makes it Problematic?'];
-                    let categories = slurDetails['Categories'];
-                    let htmlContent = ``;
-                    if (levelOfSeverity) {
-                        htmlContent += `<p><span class="label"><b>Level of Severity:</b></span> ${levelOfSeverity}</p>`;
-                    }
-                    if (casual) {
-                        htmlContent += `<p><span class="label"><b>Casual:</b></span> ${casual}</p>`;
-                    }
-                    if (approapriated) {
-                        htmlContent += `<p><span class="label"><b>Appropriated:</b></span> ${approapriated}</p>`;
-                    }
-                    if (reason) {
-                        htmlContent += `<p><span class="label"><b>If, Appropriated, Is it by Community or Others?:</b></span> ${reason}</p>`;
-                    }
-                    if (problematic) {
-                        htmlContent += `<p><span class="label"><b>What Makes it Problematic?:</b></span> ${problematic}</p>`;
-                    }
-                    if (categories.length > 0) {
-                        htmlContent += `<p><span class="label"><b>Categories:</b></span> ${slurDetails[
-                            'Categories'
-                        ].join(', ')}</p>`;
-                    }
-                    span.innerHTML = htmlContent;
-                }
-            });
-
-
-
-            // sup.appendChild(span)
-
-            // element.append(sup)
-            // element.append(img)
-            // let sups = element.children[0]
-            // let spans = element.children[0].children[1]
-            // // const svgs = element.children[0].children[0];
-            // const svgs = element.children[element.children.length-1];
-            // svgs.addEventListener('mouseover', function () {
-            //     sups.children[0].style.display = "inline-block"
-            // });
-
-            // svgs.addEventListener('mouseout', function () {
-            //     sups.children[0].style.display = "none"
-            // });
-
-            sup.appendChild(span);
-
-            // console.log("Element first child",element.children[0])
-            // console.log("Element last child",element.children[element.children.length-1])
-            // console.log("SUP: ",sup)
-            // console.log("ELEMENT IS: ",element)
-            // console.log("ELEMENT INNERHTML: ",element.innerHTML)
-
-            element.append(span);
-
-            // console.log("ELEMENT AFTER IS: ",element)
-            // element.append(img)
-            let slur = element.children[0];
+            // Add hover styles
             slur.style.backgroundColor = '#ffde2155';
             slur.style.boxShadow = '0px 0px 5px #ffde21';
             slur.style.cursor = 'pointer';
 
-            let metabox = element.children[1];
-            // console.log("METABOX IS: ",metabox)
-            let spans = element.children[0].children[1];
-            // const svgs = element.children[0].children[0];
-            const svgs = element.children[element.children.length - 1];
-            slur.addEventListener('mouseover', function () {
-                metabox.style.display = 'inline-block';
-            });
+            // Create a single tooltip container and React root if not already created
+            let tooltipContainer = document.getElementById('slur-tooltip-container');
+            if (!tooltipContainer) {
+                tooltipContainer = document.createElement('div');
+                tooltipContainer.id = 'slur-tooltip-container';
+                tooltipContainer.style.position = 'absolute';
+                tooltipContainer.style.zIndex = '1000000000';
+                document.body.appendChild(tooltipContainer);
+                tooltipContainer.root = createRoot(tooltipContainer);
+            }
 
-            slur.addEventListener('mouseout', function () {
-                metabox.style.display = 'none';
-            });
+            // Find the slur details from jsonData
+            const matchedSlur = jsonData.find(slur => 
+                Object.keys(slur)[0].toLowerCase() === targetWord.toLowerCase()
+            );
+            const slurDetails = matchedSlur ? matchedSlur[Object.keys(matchedSlur)[0]] : {};          
+
+            const handleMouseOver = () => {
+                const rect = slur.getBoundingClientRect();
+                tooltipContainer.style.top = `${rect.bottom + window.scrollY}px`;
+                tooltipContainer.style.left = `${rect.left + window.scrollX}px`;
+
+                tooltipContainer.root.render(
+                    <HoverSlurMetadata slurDetails={slurDetails} />
+                );
+            };
+
+            const handleMouseOut = () => {
+                tooltipContainer.root.render(null);
+            };
+
+            slur.addEventListener('mouseover', handleMouseOver);
+            slur.addEventListener('mouseout', handleMouseOut);
         });
     });
 }
@@ -304,9 +205,4 @@ function addMetaData(targetWords, jsonData) {
 export default {
     processNewlyAddedNodesGeneral,
     processNewlyAddedNodesGeneral2,
-    addMetaData,
-    locateSlur,
-    findPositions,
-    getAllTextNodes,
-    checkFalseTextNode
 };
---
+++ browser-extension/plugin/src/ui-components/atoms/HoverSlurMetadata.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Box, Text } from 'grommet';
+
+const HoverCategoryBubble = ({ data }) => {
+    return (
+        <Box>
+            <Box
+                round="small"
+                background="#FADA5E"
+                pad="xsmall"
+                width="fit-content"
+                wrap={true}
+                style={{
+                    fontFamily: 'Raleway',
+                    fontSize: '9px',
+                    color: 'black',
+                }}
+            >
+                <Text size="small">{data}</Text>
+            </Box>
+        </Box>
+    );
+};
+
+const HoverSlurMetadata = ({ slurDetails }) => {
+    return (
+        <Box
+            pad="small"
+            background="antiquewhite"
+            border={{ color: 'black', size: 'xsmall' }}
+            round="small"
+            width="18rem"
+            justify="start"
+            align="start"
+            elevation="small"
+            style={{
+                fontFamily: 'Raleway',
+                fontSize: '9px',
+                color: 'black',
+                textAlign: 'left',
+                lineHeight: '1.5',
+                minWidth: "288px"
+            }}
+        >
+            {slurDetails['Level of Severity'] && (
+                <Text>
+                    <b>Level of Severity:</b> {slurDetails['Level of Severity']}
+                </Text>
+            )}
+            {slurDetails['Casual'] && (
+                <Text>
+                    <b>Casual:</b> {slurDetails['Casual']}
+                </Text>
+            )}
+            {slurDetails['Appropriated'] && (
+                <Text>
+                    <b>Appropriated:</b> {slurDetails['Appropriated']}
+                </Text>
+            )}
+            {slurDetails['If, Appropriated, Is it by Community or Others?'] && (
+                <Text>
+                    <b>If, Appropriated, Is it by Community or Others?:</b>{' '}
+                    {
+                        slurDetails[
+                            'If, Appropriated, Is it by Community or Others?'
+                        ]
+                    }
+                </Text>
+            )}
+            {slurDetails['What Makes it Problematic?'] && (
+                <Text>
+                    <b>What Makes it Problematic?:</b>{' '}
+                    {slurDetails['What Makes it Problematic?']}
+                </Text>
+            )}
+
+            {slurDetails['Categories'] &&
+                slurDetails['Categories'].length > 0 && (
+                    <Box>
+                        <Text>
+                            <b>Categories:</b>
+                        </Text>
+                        <Box direction="row" gap="xsmall" cssGap wrap>
+                            {slurDetails['Categories'].map(
+                                (category, index) => (
+                                    <HoverCategoryBubble
+                                        key={index}
+                                        data={category.replace(/_/g, ' ')}
+                                    />
+                                )
+                            )}
+                        </Box>
+                    </Box>
+                )}
+        </Box>
+    );
+};
+
+export default HoverSlurMetadata;
