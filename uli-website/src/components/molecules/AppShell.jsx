ac55067a7cd95ddba6814e3b8837c4894375eb52
maanasb01 <maanasb01@gmail.com>
2025-04-24 15:59:05 +0530

    chore: add seo title

--- uli-website/src/components/molecules/AppShell.jsx
+++ uli-website/src/components/molecules/AppShell.jsx
@@ -6,6 +6,7 @@ import NavBar from "./NavBar";
 import i18n from "../atoms/i18n";
 import { useTranslation } from "react-i18next";
 import Footer from "./Footer";
+import { useLocation } from "@reach/router";
 
 export default function AppShell({ children }) {
   const { t, i18n } = useTranslation();
@@ -13,17 +14,41 @@ export default function AppShell({ children }) {
     const lang = localStorage.getItem("uli-lang");
     i18n.changeLanguage(lang);
   }, []);
+  const location = useLocation();
 
+  let fullPath = location.pathname;
+  if (fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
+  }
+  let title = fullPath.split("/").at(-1);
+
+  if (["hi", "en", "ta", "ma"].includes(title)) {
+    title = "Uli";
+  }
+
+  title = formatTitle(title);
+
+  function formatTitle(str) {
+    return str
+      .split("-")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
   return (
     <Grommet theme={Theme}>
       <main
-        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
       >
         <Helmet>
           <meta charSet="utf-8" />
           <title>Uli</title>
           <link rel="stylesheet" href="/layout.css" />
           <link rel="stylesheet" href="https://use.typekit.net/twt1ywc.css" />
+          <meta property="og:title" content={title} />
           <meta name="icon" href="images/favicon-32x32.png" />
 
           <script
