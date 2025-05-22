57aa9a841970865c43fe36e67bce79e7e2377d3d
Mahimahto <143989024+Mahimahto@users.noreply.github.com>
2025-05-22 15:18:14 +0530

    chore: render pie chart using elixir map data (#774)

--- uli-community/lib/uli_community_web/controllers/dashboard_controller.ex
+++ uli-community/lib/uli_community_web/controllers/dashboard_controller.ex
@@ -2,6 +2,17 @@ defmodule UliCommunityWeb.DashboardController do
   use UliCommunityWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index, layout: false)
+    pie_data = [
+      %{label: "RED", value: 40},
+      %{label: "Blue", value: 25},
+      %{label: "Yellow", value: 35},
+      %{label: "Pink", value: 20},
+      %{label: "Green", value: 60},
+      %{label: "Black", value: 70},
+      %{label: "Purple", value: 90},
+      %{label: "Orange", value: 120}
+    ]
+
+    render(conn, :index, pie_data: pie_data, layout: false)
   end
 end
