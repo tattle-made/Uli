c9f921eff84493931c809f4d5a5be2c5572936e0
Aatman Vaidya <aatmanvaidya@gmail.com>
2025-05-28 20:34:54 +0530

    feat: video worker setup via oban

--- uli-community/config/config.exs
+++ uli-community/config/config.exs
@@ -10,7 +10,7 @@ import Config
 config :uli_community, Oban,
   engine: Oban.Engines.Basic,
   notifier: Oban.Notifiers.Postgres,
-  queues: [default: 10],
+  queues: [index: 10],
   repo: UliCommunity.Repo
 
 config :uli_community,
