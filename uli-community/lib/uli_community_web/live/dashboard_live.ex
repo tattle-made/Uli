166f78f716e674a7fa5426f31610cb0ba069c5df
Mahimahto <143989024+Mahimahto@users.noreply.github.com>
2025-06-17 14:07:38 +0530

    feat: pie chart to visualise slur distribution by level of severity (#797)
    * Add pie chart to visualize slur distribution by severity using D3.js
    
    * Improve dashboard layout: added titles to bar and pie charts, increased spacing, and aligned chart height
    
    * Handle DB fetch errors in DashboardLive with try/rescue and fallback UI

--- uli-community/lib/uli_community_web/live/dashboard_live.ex
+++ uli-community/lib/uli_community_web/live/dashboard_live.ex
@@ -6,12 +6,39 @@ defmodule UliCommunityWeb.DashboardLive do
 
   def mount(_params, _session, socket) do
     if connected?(socket), do: Phoenix.PubSub.subscribe(UliCommunity.PubSub, @topic)
-    slurs = UserContribution.get_top_slurs(10)
-    {:ok, assign(socket, slurs: slurs)}
+
+    slurs =
+      try do
+        UserContribution.get_top_slurs(10)
+      rescue
+        _ -> nil
+      end
+
+    severity_data =
+      try do
+        UserContribution.get_severity_distribution()
+      rescue
+        _ -> nil
+      end
+
+    {:ok, assign(socket, slurs: slurs, severity_data: severity_data)}
   end
 
   def handle_info(:slur_changed, socket) do
-    slurs = UserContribution.get_top_slurs(10)
-    {:noreply, assign(socket, slurs: slurs)}
+    slurs =
+      try do
+        UserContribution.get_top_slurs(10)
+      rescue
+        _ -> nil
+      end
+
+    severity_data =
+      try do
+        UserContribution.get_severity_distribution()
+      rescue
+        _ -> nil
+      end
+
+    {:noreply, assign(socket, slurs: slurs, severity_data: severity_data)}
   end
 end
