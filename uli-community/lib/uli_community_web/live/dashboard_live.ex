6c9c24f6d73d3de5acca4f3130612010589a40b2
Manisha Kumari <manisha.tkg08@gmail.com>
2025-06-17 11:45:44 +0530

    Handle DB fetch errors in DashboardLive with try/rescue and fallback UI

--- uli-community/lib/uli_community_web/live/dashboard_live.ex
+++ uli-community/lib/uli_community_web/live/dashboard_live.ex
@@ -7,15 +7,37 @@ defmodule UliCommunityWeb.DashboardLive do
   def mount(_params, _session, socket) do
     if connected?(socket), do: Phoenix.PubSub.subscribe(UliCommunity.PubSub, @topic)
 
-    slurs = UserContribution.get_top_slurs(10)
-    severity_data = UserContribution.get_severity_distribution()
+    slurs =
+      try do
+        UserContribution.get_top_slurs(10)
+      rescue
+        _ -> nil
+      end
+
+    severity_data =
+      try do
+        UserContribution.get_severity_distribution()
+      rescue
+        _ -> nil
+      end
 
     {:ok, assign(socket, slurs: slurs, severity_data: severity_data)}
   end
 
   def handle_info(:slur_changed, socket) do
-    slurs = UserContribution.get_top_slurs(10)
-    severity_data = UserContribution.get_severity_distribution()
+    slurs =
+      try do
+        UserContribution.get_top_slurs(10)
+      rescue
+        _ -> nil
+      end
+
+    severity_data =
+      try do
+        UserContribution.get_severity_distribution()
+      rescue
+        _ -> nil
+      end
 
     {:noreply, assign(socket, slurs: slurs, severity_data: severity_data)}
   end
