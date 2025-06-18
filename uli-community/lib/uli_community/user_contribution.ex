2062d86458958912f0c7ae16c829110715e88eb2
Aatman Vaidya <aatmanvaidya@gmail.com>
2025-06-18 15:54:24 +0530

    feat: setup pagination

--- uli-community/lib/uli_community/user_contribution.ex
+++ uli-community/lib/uli_community/user_contribution.ex
@@ -22,6 +22,142 @@ defmodule UliCommunity.UserContribution do
   end
 
   @doc """
+  Returns paginated and filtered list of crowdsourced_slurs.
+  """
+  def list_crowdsourced_slurs_with_filters(search_params) do
+    page_num = Keyword.get(search_params, :page_num, 1)
+    per_page = 20
+    offset = (page_num - 1) * per_page
+
+    query = build_filtered_query(search_params)
+
+    # Get total count
+    count_query = from(s in query, select: count(s.id))
+    total_count = Repo.one(count_query)
+
+    # Get paginated results
+    results_query =
+      query
+      |> apply_sorting(search_params)
+      |> limit(^per_page)
+      |> offset(^offset)
+
+    results = Repo.all(results_query)
+
+    {total_count, results}
+  end
+
+  defp build_filtered_query(search_params) do
+    CrowdsourcedSlur
+    |> filter_by_level_of_severity(search_params)
+    |> filter_by_casual(search_params)
+    |> filter_by_appropriated(search_params)
+    |> filter_by_source(search_params)
+    |> filter_by_date_range(search_params)
+    |> filter_by_categories(search_params)
+  end
+
+  defp apply_sorting(query, search_params) do
+    case Keyword.get(search_params, :sort) do
+      "oldest" -> order_by(query, [s], asc: s.inserted_at)
+      "newest" -> order_by(query, [s], desc: s.inserted_at)
+      _ -> order_by(query, [s], desc: s.inserted_at)
+    end
+  end
+
+  defp filter_by_level_of_severity(query, search_params) do
+    case Keyword.get(search_params, :level_of_severity) do
+      "all" ->
+        query
+
+      level when level in ["low", "medium", "high"] ->
+        from(s in query, where: s.level_of_severity == ^String.to_existing_atom(level))
+
+      _ ->
+        query
+    end
+  end
+
+  defp filter_by_casual(query, search_params) do
+    case Keyword.get(search_params, :casual) do
+      "all" -> query
+      "true" -> from(s in query, where: s.casual == true)
+      "false" -> from(s in query, where: s.casual == false)
+      _ -> query
+    end
+  end
+
+  defp filter_by_appropriated(query, search_params) do
+    case Keyword.get(search_params, :appropriated) do
+      "all" -> query
+      "true" -> from(s in query, where: s.appropriated == true)
+      "false" -> from(s in query, where: s.appropriated == false)
+      _ -> query
+    end
+  end
+
+  defp filter_by_source(query, search_params) do
+    case Keyword.get(search_params, :source) do
+      "all" ->
+        query
+
+      source when source in ["plugin", "experts_2022", "crowdsourcing_exercise"] ->
+        from(s in query, where: s.source == ^String.to_existing_atom(source))
+
+      _ ->
+        query
+    end
+  end
+
+  defp filter_by_date_range(query, search_params) do
+    from_date = Keyword.get(search_params, :from)
+    to_date = Keyword.get(search_params, :to)
+
+    query
+    |> maybe_filter_by_date(from_date, to_date)
+  end
+
+  defp maybe_filter_by_date(query, nil, nil), do: query
+
+  defp maybe_filter_by_date(query, from_date, nil) do
+    case Date.from_iso8601(from_date) do
+      {:ok, date} -> from(s in query, where: fragment("DATE(?)", s.inserted_at) >= ^date)
+      _ -> query
+    end
+  end
+
+  defp maybe_filter_by_date(query, nil, to_date) do
+    case Date.from_iso8601(to_date) do
+      {:ok, date} -> from(s in query, where: fragment("DATE(?)", s.inserted_at) <= ^date)
+      _ -> query
+    end
+  end
+
+  defp maybe_filter_by_date(query, from_date, to_date) do
+    case {Date.from_iso8601(from_date), Date.from_iso8601(to_date)} do
+      {{:ok, from}, {:ok, to}} ->
+        from(s in query,
+          where:
+            fragment("DATE(?)", s.inserted_at) >= ^from and
+              fragment("DATE(?)", s.inserted_at) <= ^to
+        )
+
+      _ ->
+        query
+    end
+  end
+
+  defp filter_by_categories(query, search_params) do
+    categories = Keyword.get(search_params, :categories, [])
+
+    if Enum.empty?(categories) do
+      query
+    else
+      from(s in query, where: fragment("? && ?", s.categories, ^categories))
+    end
+  end
+
+  @doc """
   Gets a single crowdsourced_slur.
 
   Raises `Ecto.NoResultsError` if the Crowdsourced slur does not exist.
