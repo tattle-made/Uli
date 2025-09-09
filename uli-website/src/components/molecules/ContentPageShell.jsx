9fe2bd19187aedfd1d60df0511454431c8635c29
maanasb01 <maanasb01@gmail.com>
2025-09-09 13:54:42 +0530

    fix: mdx syntax according to mdx-3 in mdx files & layout fix in ContentPageShell

--- uli-website/src/components/molecules/ContentPageShell.jsx
+++ uli-website/src/components/molecules/ContentPageShell.jsx
@@ -5,18 +5,18 @@ import { MDXProvider } from "@mdx-js/react";
 import styled from "styled-components";
 import CustomCodeBlock from "./codeSnippet";
 
-const HeadingOne = styled(Text)`
-  font-size: 2em;
-  line-height: 1em;
-`;
+// const HeadingOne = styled(Text)`
+//   font-size: 2em;
+//   line-height: 1em;
+// `;
 
-const HeadingTwo = styled(Text)`
-  font-size: 1.6em;
-`;
+// const HeadingTwo = styled(Text)`
+//   font-size: 1.6em;
+// `;
 
-const HeadingThree = styled(Text)`
-  font-size: 1.2em;
-`;
+// const HeadingThree = styled(Text)`
+//   font-size: 1.2em;
+// `;
 
 const ContentPageShell = ({ children }) => {
   return (
@@ -25,9 +25,27 @@ const ContentPageShell = ({ children }) => {
         <MDXProvider
           components={{
             // Map HTML element tag to React component
-            h1: HeadingOne,
-            h2: HeadingTwo,
-            h3: HeadingThree,
+            h1: (props)=> <h1 style={{lineHeight: "2.5rem"}} {...props}/>,
+            h2: (props)=> <h2 style={{lineHeight: "2rem"}} {...props}/>,
+            // h3: HeadingThree,
+            p: (props) => <Text style={{ wordBreak: 'break-word' }} {...props} />,
+            li: (props) => (
+              <li>
+                <Text style={{ wordBreak: 'break-word' }} {...props} />
+              </li>
+            ),
+            a: (props) => (
+              <a
+                {...props}
+                style={{
+                  wordBreak: 'break-all',
+                  // color: '#7D4CDB',
+                  // textDecoration: 'none'
+                }}
+                target="_blank"
+                rel="noopener noreferrer"
+              />
+            ),
             code: (props) => <CustomCodeBlock {...props} />,
           }}
         >
