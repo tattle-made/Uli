f0192277d79321ee38922a654963473cdd12fd77
Mahimahto <143989024+Mahimahto@users.noreply.github.com>
2025-09-29 11:34:00 +0530

    feat: Make Crowdsource Contributions page responsive for all screens (#828)
    * Make search bar, sidebar, and 'Showing all contributions' section responsive
    * Remove 'Apply Filter' button and freeze table columns/rows on mobile screens

--- uli-community/lib/uli_community_web/components/core_components.ex
+++ uli-community/lib/uli_community_web/components/core_components.ex
@@ -478,57 +478,81 @@ defmodule UliCommunityWeb.CoreComponents do
   slot :action, doc: "the slot for showing user actions in the last table column"
 
   def table(assigns) do
-    assigns =
-      with %{rows: %Phoenix.LiveView.LiveStream{}} <- assigns do
-        assign(assigns, row_id: assigns.row_id || fn {id, _item} -> id end)
-      end
+  assigns =
+    with %{rows: %Phoenix.LiveView.LiveStream{}} <- assigns do
+      assign(assigns, row_id: assigns.row_id || fn {id, _item} -> id end)
+    end
 
-    ~H"""
-    <div class="overflow-y-auto px-4 sm:overflow-visible sm:px-0">
-      <table class="w-[40rem] mt-11 sm:w-full">
-        <thead class="text-sm text-center leading-6 text-zinc-500">
-          <tr>
-            <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal"><%= col[:label] %></th>
-            <th :if={@action != []} class="relative p-0 pb-4">
-              <span class="sr-only"><%= gettext("Actions") %></span>
-            </th>
-          </tr>
-        </thead>
-        <tbody
-          id={@id}
-          phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
-          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
+  ~H"""
+  <div class="overflow-x-auto overflow-y-auto max-h-[500px] px-4 sm:overflow-visible sm:px-0">
+    <table class="w-[40rem] mt-11 sm:w-full border-collapse">
+      <thead class="text-sm text-center leading-6 text-zinc-500">
+        <tr>
+          <th
+            :for={{col, i} <- Enum.with_index(@col)}
+            class={[
+              "p-0 pb-4 pr-6 font-normal",
+              "max-sm:sticky max-sm:top-0 max-sm:bg-white max-sm:z-20",
+              i == 0 && "max-sm:left-0 max-sm:z-30"
+            ]}
+          >
+            <%= col[:label] %>
+          </th>
+          <th
+            :if={@action != []}
+            class="relative p-0 pb-4 max-sm:sticky max-sm:top-0 max-sm:z-20 max-sm:bg-white"
+          >
+            <span class="sr-only"><%= gettext("Actions") %></span>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody
+        id={@id}
+        phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
+        class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
+      >
+        <tr
+          :for={row <- @rows}
+          id={@row_id && @row_id.(row)}
+          class="group hover:bg-slate-200"
         >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-slate-200">
-            <td
-              :for={{col, i} <- Enum.with_index(@col)}
-              phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0 text-center", @row_click && "hover:cursor-pointer"]}
-            >
-              <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-slate-200 sm:rounded-l-xl" />
-                <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
-                  <%= render_slot(col, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-            <td :if={@action != []} class="relative w-14 p-0">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-slate-200 sm:rounded-r-xl" />
-                <span
-                  :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                >
-                  <%= render_slot(action, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    """
-  end
+          <td
+            :for={{col, i} <- Enum.with_index(@col)}
+            phx-click={@row_click && @row_click.(row)}
+            class={[
+              "relative p-0 text-center",
+              @row_click && "hover:cursor-pointer",
+              # first column सिर्फ phone पर sticky
+              i == 0 && "max-sm:sticky max-sm:left-0 max-sm:z-20 max-sm:bg-white"
+            ]}
+          >
+            <div class="block py-4 pr-6">
+              <span class=" right-0 -left-4 group-hover:bg-slate-200 sm:rounded-l-xl" />
+              <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
+                <%= render_slot(col, @row_item.(row)) %>
+              </span>
+            </div>
+          </td>
+
+          <td :if={@action != []} class="relative w-14 p-0">
+            <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
+              <span class="-right-4 left-0 group-hover:bg-slate-200 sm:rounded-r-xl" />
+              <span
+                :for={action <- @action}
+                class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
+              >
+                <%= render_slot(action, @row_item.(row)) %>
+              </span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  """
+end
+
 
   @doc """
   Renders a data list.
