36ec1658a2ebb25cdb991d13411e86d545eeb1c6
Manisha Kumari <manisha.tkg08@gmail.com>
2026-01-23 14:14:41 +0530

    Create dummy /new-home route for upcoming homepage design

---
+++ uli-website/src/pages/new-home.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const NewHome = () => {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>New Home</h1>
+      <p>This is a temporary placeholder for the new homepage design.</p>
+    </main>
+  )
+}
+
+export default NewHome
