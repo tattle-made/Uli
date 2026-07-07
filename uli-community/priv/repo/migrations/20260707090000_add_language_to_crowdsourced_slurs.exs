77062153c9a01e11d46a22c94b9f6d2e9077d149
Yashasvi Nancherla <yash01699@gmail.com>
2026-07-07 20:39:05 +0530

    Adds reversible rollback clause in language migration

--- uli-community/priv/repo/migrations/20260707090000_add_language_to_crowdsourced_slurs.exs
+++ uli-community/priv/repo/migrations/20260707090000_add_language_to_crowdsourced_slurs.exs
@@ -2,7 +2,10 @@ defmodule UliCommunity.Repo.Migrations.AddLanguageToCrowdsourcedSlurs do
   use Ecto.Migration
 
   def change do
-    execute("CREATE TYPE language_enum AS ENUM ('hindi', 'english', 'tamil', 'bengali', 'malayalam')")
+    execute(
+      "CREATE TYPE language_enum AS ENUM ('hindi', 'english', 'tamil', 'bengali', 'malayalam')",
+      "DROP TYPE language_enum"
+    )
 
     alter table(:crowdsourced_slurs) do
       add(:language, :language_enum)
